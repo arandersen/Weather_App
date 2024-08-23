7dfcc213a6ff4fbaf52024ce9b8e451ccba6a2f6
Acezz4120 <arandersen@ucsd.edu>
2024-08-22 17:39:44 -0700

    FrontEnd
    Simple weather app using both GEOAPI and weatherAPI

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'c11d840505mshdb364c0bd10254cp1dfd5djsn879965441341',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3d32d60df8e83be432e763969b3641b0"; 
